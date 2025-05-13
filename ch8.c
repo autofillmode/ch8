deb96d6b90958f41e495dee6822120a87c2f21e5
autofillmode <axel.hartikainen1@gmail.com>
2025-05-13 12:20:30 +0300

    refactor: move processing to own function

--- ch8.c
+++ ch8.c
@@ -26,7 +26,18 @@
 
 #define FNT_START 0x50
 
+/* Globals for simplicity */
+uint16_t I; /* Index register */
+uint16_t call_stack[STACK_SIZE], stackp;
+uint8_t registers[16]; /* Registers V0 through VF */
+uint8_t screen[SCREEN_W][SCREEN_H] = { 0 };
+uint8_t memory[MEMB];                    /* 4096 Bytes of ram */
+uint8_t *startptr = memory + START_ADDR; /* the ROM is loaded here */
+int pc = START_ADDR;                     /* The program counter */
+int last_goto;    /* Last goto, useful for not printing inf. loops */
+int drawFlag = 0; /* whether any drawing is to be done */
 void drawScreen (SDL_Renderer *renderer, uint8_t screen[SCREEN_W][SCREEN_H]);
+void processCycle (void);
 
 int
 main (int argc, char *argv[])
@@ -36,16 +47,6 @@ main (int argc, char *argv[])
   SDL_Window *gWindow = NULL;
   SDL_Renderer *renderer = NULL;
   FILE *rom;
-
-  uint8_t screen[SCREEN_W][SCREEN_H] = { 0 };
-
-  uint16_t I; /* Index register */
-  uint16_t call_stack[STACK_SIZE], stackp;
-  uint8_t registers[16];                   /* Registers V0 through VF */
-  uint8_t memory[MEMB];                    /* 4096 Bytes of ram */
-  uint8_t *startptr = memory + START_ADDR; /* the ROM is loaded here */
-  int pc = START_ADDR;                     /* The program counter */
-  int last_goto; /* Last goto, useful for not printing inf. loops */
 
   uint8_t fnt[80] = {
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -89,6 +90,7 @@ main (int argc, char *argv[])
 
   SDL_CreateWindowAndRenderer (WINDOW_W, WINDOW_H, SDL_WINDOW_SHOWN, &gWindow,
                                &renderer);
+  SDL_SetRenderDrawColor (renderer, 0, 0, 0, 255);
 
   int quit = 0;
   SDL_Event e;
@@ -102,255 +104,12 @@ main (int argc, char *argv[])
               quit = 1;
             }
         }
-      SDL_SetRenderDrawColor (renderer, 0, 0, 0, 255);
+      processCycle ();
       SDL_RenderClear (renderer);
-      drawScreen (renderer, screen);
+      if (drawFlag)
+        drawScreen (renderer, screen);
       SDL_RenderPresent (renderer);
-
-      uint8_t x, y, n, nn, opcode;
-      uint16_t nnn, instruction;
-
-      instruction = (memory[pc] << 8) | memory[pc + 1];
-      opcode = (instruction & 0xF000) >> 12;
-      x = (instruction & 0x0F00) >> 8;
-      y = (instruction & 0x00F0) >> 4;
-      n = instruction & 0x000F;
-      nn = (instruction & 0x00FF);
-      nnn = (instruction & 0x0FFF);
-
-      pc += 2;
-
-      switch (opcode)
-        {
-        case 0x0:
-          {
-            if (nn == 0x00)
-              {
-                memset (screen, 0, sizeof (screen));
-                printf ("0x%x: CLS\n", pc); /* clear the screen */
-              }
-            else if (nn == 0xEE)
-              {
-                pc = call_stack[--stackp]; /* return  */
-                printf ("0x%x: RETURN\n", pc);
-              }
-          }
-          break;
-        case 0x1:
-          {
-            int old = pc;
-            pc = nnn;
-            if (last_goto != nnn) /* don't print infinite loops */
-              printf ("0x%x: GOTO 0x%x\n", old, pc);
-            last_goto = nnn;
-          }
-          break;
-        case 0x2:
-          {
-            call_stack[stackp] = pc;
-            ++stackp;
-            pc = nnn;
-          }
-          break;
-        case 0x3:
-          {
-            if (registers[x] == nn)
-              pc += 2;
-          }
-          break;
-        case 0x4:
-          {
-            if (registers[x] != nn)
-              pc += 2;
-          }
-          break;
-        case 0x5:
-          {
-            if (registers[x] == registers[y])
-              pc += 2;
-          }
-          break;
-        case 0x6:
-          {
-            registers[x] = nn;
-            printf ("0x%x: V%x = 0x%x\n", pc, x, nn);
-          }
-          break;
-        case 0x7:
-          {
-            registers[x] += nn;
-            printf ("0x%x: V%x += 0x%x = %x\n", pc, x, nn, registers[x]);
-          }
-          break;
-        case 0x8:
-          {
-            printf ("0x%x:", pc);
-            switch (n)
-              {
-              case 0x0:
-                {
-                  printf ("V%x = V%x\n", x, y);
-                  registers[x] = registers[y];
-                }
-                break;
-              case 0x1:
-                {
-                  printf ("V%x OR V%x\n", x, y);
-                  registers[x] |= registers[y];
-                }
-                break;
-              case 0x2:
-                {
-                  printf ("V%x =AND V%x\n", x, y);
-                  registers[x] &= registers[y];
-                }
-                break;
-              case 0x3:
-                {
-                  printf ("V%x =XOR V%x\n", x, y);
-                  registers[x] ^= registers[y];
-                }
-                break;
-              case 0x4:
-                {
-                  uint16_t sum;
-                  printf ("V%x += V%x\n", x, y);
-                  sum = registers[x] += registers[y];
-                  if (sum > 255)
-                    registers[0xF] = 1;
-                  else
-                    registers[0xF] = 0;
-                }
-                break;
-              case 0x5:
-                {
-                  printf ("V%x -= V%x\n", x, y);
-                  registers[x] -= registers[y];
-                }
-                break;
-              case 0x6:
-                {
-                  printf ("V%x >> 1\n", x);
-                  registers[x] = registers[x] >> 1;
-                }
-                break;
-              case 0x7:
-                {
-                  printf ("V%x = V%x -V%x\n", x, x, y);
-                  registers[x] = registers[y] - registers[x];
-                }
-                break;
-              case 0xE:
-                {
-                  printf ("V%x << 1\n", x);
-                  registers[x] = registers[x] << 1;
-                }
-                break;
-              }
-          }
-          break;
-        case 0x9:
-          {
-            if (registers[x] != registers[y])
-              pc += 2;
-          }
-          break;
-        case 0xA:
-          {
-            I = nnn;
-            printf ("0x%x: I = 0x%x\n", pc, nnn);
-          }
-          break;
-        case 0xB:
-          {
-            int old = pc;
-            pc = nnn + registers[0x0];
-            printf ("0x%xGOTO-OFFSET pc = 0x%x", old, pc);
-          }
-          break;
-        case 0xC:
-          {
-            registers[x] = (rand () % nn) & nn;
-          }
-        case 0xD:
-          printf ("0x%x: DISPLAY\n", pc);
-          {
-            uint8_t p_x = registers[x] % 64;
-            uint8_t p_y = registers[y] % 32;
-            registers[0xF] = 0;
-            int index = I;
-
-            for (unsigned int r = 0; r < n; ++r)
-              {
-                uint8_t sprite = memory[index + r];
-
-                for (unsigned char c = 0; c < 8; ++c)
-                  {
-                    uint8_t spritepxl = sprite & (0x80u >> c);
-                    uint8_t screenpxl = screen[p_x + c][p_y + r];
-                    if (spritepxl)
-                      {
-                        if (screenpxl)
-                          {
-                            registers[0xF] = 1;
-                            screen[p_x + c][p_y + r] = 0;
-                          }
-                        else
-                          {
-                            screen[p_x + c][p_y + r] = 1;
-                          }
-                      }
-                  }
-              }
-          }
-          break;
-        case 0xF:
-          {
-            switch (nn) /* second byte */
-              {
-              case 0x1E:
-                {
-                  I += x;
-                }
-                break;
-              case 0x29:
-                {
-                  /* each font takes 5 bytes, that's why * 5 */
-                  I = FNT_START + ((registers[x] & 0xF) * 5);
-                }
-                break;
-              case 0x33:
-                {
-                  uint8_t in = registers[x];
-                  memory[I] = in / 100;
-                  in = in % 100;
-                  memory[I + 1] = in / 10;
-                  in = in % 10;
-                  memory[I + 2] = in;
-                }
-                break;
-                /* TODO: toggle to count by mutating value of I */
-              case 0x55:
-                {
-                  for (int i = 0; i <= x; i++)
-                    {
-                      memory[I + i] = registers[i];
-                    }
-                }
-                break;
-              case 0x65:
-                {
-                  for (int i = 0; i <= x; i++)
-                    {
-                      registers[i] = memory[I + i];
-                    }
-                }
-                break;
-              }
-          }
-          break;
-        }
-      SDL_Delay (50);
+      SDL_Delay (5);
       if (pc == (START_ADDR + romlen))
         SDL_Quit ();
     }
@@ -368,5 +127,254 @@ drawScreen (SDL_Renderer *renderer, uint8_t screen[64][32])
           SDL_Rect rect = { x * 10, y * 10, 10, 10 };
           SDL_RenderFillRect (renderer, &rect);
         }
+    }
+}
+
+void
+processCycle (void)
+{
+  uint8_t x, y, n, nn, opcode;
+  uint16_t nnn, instruction;
+
+  instruction = (memory[pc] << 8) | memory[pc + 1];
+  opcode = (instruction & 0xF000) >> 12;
+  x = (instruction & 0x0F00) >> 8;
+  y = (instruction & 0x00F0) >> 4;
+  n = instruction & 0x000F;
+  nn = (instruction & 0x00FF);
+  nnn = (instruction & 0x0FFF);
+
+  pc += 2;
+
+  switch (opcode)
+    {
+    case 0x0:
+      {
+        if (nn == 0x00)
+          {
+            memset (screen, 0, sizeof (screen));
+            printf ("0x%x: CLS\n", pc); /* clear the screen */
+          }
+        else if (nn == 0xEE)
+          {
+            pc = call_stack[--stackp]; /* return  */
+            printf ("0x%x: RETURN\n", pc);
+          }
+      }
+      break;
+    case 0x1:
+      {
+        int old = pc;
+        pc = nnn;
+        if (last_goto != nnn) /* don't print infinite loops */
+          printf ("0x%x: GOTO 0x%x\n", old, pc);
+        last_goto = nnn;
+      }
+      break;
+    case 0x2:
+      {
+        call_stack[stackp] = pc;
+        ++stackp;
+        pc = nnn;
+      }
+      break;
+    case 0x3:
+      {
+        if (registers[x] == nn)
+          pc += 2;
+      }
+      break;
+    case 0x4:
+      {
+        if (registers[x] != nn)
+          pc += 2;
+      }
+      break;
+    case 0x5:
+      {
+        if (registers[x] == registers[y])
+          pc += 2;
+      }
+      break;
+    case 0x6:
+      {
+        registers[x] = nn;
+        printf ("0x%x: V%x = 0x%x\n", pc, x, nn);
+      }
+      break;
+    case 0x7:
+      {
+        registers[x] += nn;
+        printf ("0x%x: V%x += 0x%x = %x\n", pc, x, nn, registers[x]);
+      }
+      break;
+    case 0x8:
+      {
+        printf ("0x%x:", pc);
+        switch (n)
+          {
+          case 0x0:
+            {
+              printf ("V%x = V%x\n", x, y);
+              registers[x] = registers[y];
+            }
+            break;
+          case 0x1:
+            {
+              printf ("V%x OR V%x\n", x, y);
+              registers[x] |= registers[y];
+            }
+            break;
+          case 0x2:
+            {
+              printf ("V%x =AND V%x\n", x, y);
+              registers[x] &= registers[y];
+            }
+            break;
+          case 0x3:
+            {
+              printf ("V%x =XOR V%x\n", x, y);
+              registers[x] ^= registers[y];
+            }
+            break;
+          case 0x4:
+            {
+              uint16_t sum;
+              printf ("V%x += V%x\n", x, y);
+              sum = registers[x] += registers[y];
+              if (sum > 255)
+                registers[0xF] = 1;
+              else
+                registers[0xF] = 0;
+            }
+            break;
+          case 0x5:
+            {
+              printf ("V%x -= V%x\n", x, y);
+              registers[x] -= registers[y];
+            }
+            break;
+          case 0x6:
+            {
+              printf ("V%x >> 1\n", x);
+              registers[x] = registers[x] >> 1;
+            }
+            break;
+          case 0x7:
+            {
+              printf ("V%x = V%x -V%x\n", x, x, y);
+              registers[x] = registers[y] - registers[x];
+            }
+            break;
+          case 0xE:
+            {
+              printf ("V%x << 1\n", x);
+              registers[x] = registers[x] << 1;
+            }
+            break;
+          }
+      }
+      break;
+    case 0x9:
+      {
+        if (registers[x] != registers[y])
+          pc += 2;
+      }
+      break;
+    case 0xA:
+      {
+        I = nnn;
+        printf ("0x%x: I = 0x%x\n", pc, nnn);
+      }
+      break;
+    case 0xB:
+      {
+        int old = pc;
+        pc = nnn + registers[0x0];
+        printf ("0x%xGOTO-OFFSET pc = 0x%x", old, pc);
+      }
+      break;
+    case 0xC:
+      {
+        registers[x] = (rand () % nn) & nn;
+      }
+    case 0xD:
+      printf ("0x%x: DISPLAY\n", pc);
+      {
+        uint8_t p_x = registers[x] % 64;
+        uint8_t p_y = registers[y] % 32;
+        registers[0xF] = 0;
+        int index = I;
+
+        for (unsigned int r = 0; r < n; ++r)
+          {
+            uint8_t sprite = memory[index + r];
+
+            for (unsigned char c = 0; c < 8; ++c)
+              {
+                uint8_t spritepxl = sprite & (0x80u >> c);
+                uint8_t screenpxl = screen[p_x + c][p_y + r];
+                if (spritepxl)
+                  {
+                    if (screenpxl)
+                      {
+                        registers[0xF] = 1;
+                        screen[p_x + c][p_y + r] = 0;
+                      }
+                    else
+                      {
+                        screen[p_x + c][p_y + r] = 1;
+                      }
+                  }
+              }
+          }
+        drawFlag = 1;
+      }
+      break;
+    case 0xF:
+      {
+        switch (nn) /* second byte */
+          {
+          case 0x1E:
+            {
+              I += x;
+            }
+            break;
+          case 0x29:
+            {
+              /* each font takes 5 bytes, that's why * 5 */
+              I = FNT_START + ((registers[x] & 0xF) * 5);
+            }
+            break;
+          case 0x33:
+            {
+              uint8_t in = registers[x];
+              memory[I] = in / 100;
+              in = in % 100;
+              memory[I + 1] = in / 10;
+              in = in % 10;
+              memory[I + 2] = in;
+            }
+            break;
+            /* TODO: toggle to count by mutating value of I */
+          case 0x55:
+            {
+              for (int i = 0; i <= x; i++)
+                {
+                  memory[I + i] = registers[i];
+                }
+            }
+            break;
+          case 0x65:
+            {
+              for (int i = 0; i <= x; i++)
+                {
+                  registers[i] = memory[I + i];
+                }
+            }
+            break;
+          }
+      }
+      break;
     }
 }
